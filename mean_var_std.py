2f2413343378ee3385f3cede2cfdc6ec2b18ac43
Paine <paineplez@gmail.com>
2025-03-22 11:29:49 -0300

    data analisis - freecodecamp - 01
    calculo matricial usando numpy

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,28 +1,32 @@
+"""
+respuesta a primer problema de data analisis en freecodecamp
+recibe una lista de nueve numeros si no son nueve devuelve un error
+transforma los datos en una matriz de 3x3 realiza calculos por fila, por columna y como matriz ("flattened")
+devuelve un diccionario de listas con los resultados
+"""
 import numpy as np
 
-
 def calculate(list):
-
+    #error - si no son 9 numeros 
     if len(list) != 9: 
         raise ValueError ("List must contain nine numbers.")
-           
+    #transformacion en matriz de 3x3      
     else:
         a=np.array(list)
         a=a.reshape(3, 3)
-
-#mean
+#media - mean 
         m0 = np.mean(a, axis=0).tolist()
         m1 = np.mean(a, axis=1).tolist()
         mf = np.mean(a).tolist()
         media = [m0,m1,mf]
 
-#variance
+#varianza - variance
         v0 = np.var(a, axis=0).tolist()
         v1 = np.var(a, axis=1).tolist()
         vf = np.var(a).tolist()
         varianza = [v0,v1,vf]
 
-#standard desviation
+#desviacion estandar - standard desviation
         d0 = np.std(a, axis=0).tolist()
         d1 = np.std(a, axis=1).tolist()
         df = np.std(a).tolist()
@@ -40,12 +44,12 @@ def calculate(list):
         minf = np.min(a).tolist()
         minimo = [min0,min1,minf]
 
-#sum
+#suma
         sum0 = np.sum(a, axis=0).tolist()
         sum1 = np.sum(a, axis=1).tolist()
         sumf = np.sum(a).tolist()
         suma = [sum0,sum1,sumf]
-
+#resultados
         calculations = {
             'mean': media,
             'variance': varianza,
